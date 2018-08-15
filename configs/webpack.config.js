209f5f76001793309b98ee09ce2c56cb89e995b9
jkvim <jkvim@outlook.com>
2018-08-15 23:40:00 +0800

    chore: update webpack with html plugin

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,3 +1,17 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const NODE_ENV = process.env.NODE_ENV
+const ROOT_PATH = path.join(__dirname, '..')
+
+const getPath = (filePath) => path.resolve(ROOT_PATH, filePath)
+
 module.exports = {
-  mode: 'devlopment',
+  mode: NODE_ENV === 'production' ? 'production' : 'development',
+  entry: getPath('./src/index.js'),
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: getPath('./index.html'),
+    }),
+  ],
 }
