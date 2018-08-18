996798493de04218b3803b2afc34e2fd7e7e70d0
jkvim <jkvim@outlook.com>
2018-08-18 12:48:35 +0800

    feature: add Hello Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
+import { Hello } from './components/Hello';
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>
+    return <Hello compiler="ts-loader" framework="react" />
   }
 }
 
